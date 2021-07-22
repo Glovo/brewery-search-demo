6a0020892481ed08b68babab4cf2357c8f51c46b
Juan Carlos Arocha <juan.arocha@glovoapp.com>
2021-07-22 11:49:08 +0200

    chore: specified it blocks for BreweryListItem

--- src/components/BreweryListItem.spec.ts
+++ src/components/BreweryListItem.spec.ts
@@ -1,3 +1,9 @@
-describe('BreweryList', () => {
+describe('BreweryListItem', () => {
+  it('should display the brewery name', () => {});
 
-})
+  it('should display the brewery location {country}, {state}, {city} with icon', () => {});
+
+  it('should display the brewery phone with icon if present', () => {});
+
+  it('should display the brewery website URL with icon if present', () => {});
+});
